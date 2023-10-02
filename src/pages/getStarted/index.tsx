0871dc99127758e3ceccd239950d498b25cb9e29
BaZz <bazuaye50@gmail.com>
2023-10-02 17:10:49 +0100

    UPDATE

--- src/pages/getStarted/index.tsx
+++ src/pages/getStarted/index.tsx
@@ -52,6 +52,34 @@ export function GetStarted() {
 							<span>Continue with Google</span>
 						</Link>
 					</div>
+					<div className="social">
+						<Link
+							to={"/dashboard"}
+							className="">
+							<span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									fill="none"
+									viewBox="0 0 24 24">
+									<path
+										fill="#4285F4"
+										d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.255h5.92a5.06 5.06 0 01-2.195 3.32v2.76h3.555c2.08-1.915 3.28-4.735 3.28-8.085z"></path>
+									<path
+										fill="#34A853"
+										d="M12 23c2.97 0 5.46-.985 7.28-2.665l-3.555-2.76c-.985.66-2.245 1.05-3.725 1.05-2.865 0-5.29-1.935-6.155-4.535H2.17v2.85A10.996 10.996 0 0012 23z"></path>
+									<path
+										fill="#FBBC05"
+										d="M5.845 14.089a6.612 6.612 0 01-.345-2.09c0-.725.125-1.43.345-2.09v-2.85H2.17A10.996 10.996 0 001 11.999c0 1.775.425 3.455 1.17 4.94l3.675-2.85z"></path>
+									<path
+										fill="#EA4335"
+										d="M12 5.375c1.615 0 3.065.555 4.205 1.645l3.155-3.155C17.455 2.09 14.965 1 12 1 7.7 1 3.98 3.465 2.17 7.06l3.675 2.85C6.71 7.31 9.135 5.375 12 5.375z"></path>
+								</svg>
+							</span>
+							<span>Continue with Google</span>
+						</Link>
+					</div>
 				</div>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
