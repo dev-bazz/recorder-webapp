6a68f581388a9359d50849e4ad7b74dd6460d752
BaZz <bazuaye50@gmail.com>
2023-10-02 18:28:38 +0100

    update

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,8 +1,30 @@
 import "./style.scss";
 import logo from "./assets/helpOutDesktopLogo.png";
+import profile from "./assets/profile.png";
 import { Link } from "react-router-dom";
 
-export function Nav() {
+export function Nav({ dash = false }: { dash?: boolean }) {
+	if (dash) {
+		return (
+			<nav>
+				<div className="container nav">
+					<div className="logo">
+						<img
+							src={logo}
+							alt=""
+						/>
+					</div>
+					<Link to={"/"}>
+						<img
+							src={profile}
+							alt=""
+						/>
+					</Link>{" "}
+				</div>
+			</nav>
+		);
+	}
+
 	return (
 		<nav>
 			<div className="container nav">
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,30 @@
+import { Nav } from "../../components";
 import "./style.scss";
 
 export function DashBoard() {
-	return <div>index</div>;
+	return (
+		<div className="dash">
+			<Nav dash={true} />
+			<section className="divider">
+				<div className="container profile">
+					<div className="details">
+						<h2>Hello, John Mark</h2>
+						<p>Here are your recorded videos</p>
+					</div>
+					<div className="ctr">
+						<input
+							className="input"
+							type="text"
+						/>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<div className="container">
+					<h3>Results</h3>
+				</div>
+			</section>
+		</div>
+	);
 }
